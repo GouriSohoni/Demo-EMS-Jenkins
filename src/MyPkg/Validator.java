bb350dea3eb281da228d4cc386bdc92cf2bd6052
Gouri Sohoni <gourisohoni@hotmail.com>
2018-05-01 16:17:46 +0530

    jenkins demo

---
+++ src/MyPkg/Validator.java
@@ -0,0 +1,15 @@
+package MyPkg;
+
+public class Validator {
+	public Boolean Validate(String UserName, String Passwd)
+	{
+		if (UserName.equals("Gouri") && Passwd.equals("Sohoni"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
