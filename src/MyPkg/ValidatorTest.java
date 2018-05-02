bb0ea98abedf88993786c572ebf48fe0a959e2a1
Gouri Sohoni <gourisohoni@hotmail.com>
2018-05-02 16:26:36 +0530

    test added

---
+++ src/MyPkg/ValidatorTest.java
@@ -0,0 +1,25 @@
+package MyPkg;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+public class ValidatorTest {
+	
+	public static void main()
+	{
+		JUnitCore jCore = new JUnitCore();
+		@SuppressWarnings("unused")
+		Result result = jCore.run(ValidatorTest.class);
+		
+	}
+	@Test
+    public void TestValidate()
+	{
+		Boolean actual, expected=true;
+		Validator val = new Validator();
+		actual = val.Validate("Gouri", "Sohoni");
+		assertEquals(expected,actual);
+	}
+}
