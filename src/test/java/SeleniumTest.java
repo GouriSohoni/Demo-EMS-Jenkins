f3a6f8eca709a04a3611b655fcbe795bbfc6ead2
Gouri Sohoni <gourisohoni@hotmail.com>
2018-05-03 16:04:41 +0530

    selenium test added

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,32 @@
+package test.java;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+	WebDriver driver;
+	@Test
+		public void TestSSGSWeb() throws InterruptedException
+		{
+			System.setProperty("webdriver.chrome.driver", "e:/program files/java/eclipse/chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+			driver = new ChromeDriver(options);
+			driver.get("http://ssgs-web-app-java.azurewebsites.net/ssgs-ems-profile-editor/");
+			WebElement elementName = driver.findElement(By.name("uname"));
+			elementName.sendKeys("Gouri");
+			elementName.sendKeys(Keys.TAB);
+			WebElement elementPwd= driver.findElement(By.name("pwd"));
+			elementPwd.sendKeys("Sohoni");
+			elementPwd.sendKeys(Keys.TAB);
+			WebElement elementLogin = driver.findElement(By.name("submoit"));
+			elementLogin.sendKeys(Keys.ENTER);
+			Thread.sleep(4000);
+			driver.quit();
+		}
+}
